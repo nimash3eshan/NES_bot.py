4898b0749826b7aeee62d8d00e8ca1eba768e999
nimash eshan <81243419+nimash3eshan@users.noreply.github.com>
2021-05-04 04:08:44 +0530

    Update NES.py

--- NES.py
+++ NES.py
@@ -11,14 +11,14 @@ client=commands.Bot(command_prefix='--')
 async def info(context):
 
 
-    myEmbed = discord.Embed(title="Administrator",description="Nimash Eshan \nJohnny JTH",color=0x00ff00)
-    myEmbed.add_field(name="Partner",value="Githmi Paranahewa",inline=False)
-    myEmbed.add_field(name="Co leaders",value="Waruna Usgoda \nVasula \nnA PEACE \nRukshan \nmisha",inline=False)
+    myEmbed = discord.Embed(title="Administrator",description="Nimash Eshan \nJohnny JTH \n࿐༒ҜƗŇǤ STRÎKÊR༒࿐ ",color=0x00ff00)
+    myEmbed.add_field(name="Co leaders",value="BUNNY💓 \nWaruna Usgoda \nVasula \nnA PEACE \nmisha",inline=False)
     myEmbed.add_field(name="Vip Members",value="Trey(ナ尺ツ) \nUmal Manjitha(bota malli) \nSisitha Liyanage(Fraddy Gaming) \nBunny",inline=False)
-    myEmbed.add_field(name="Guest",value="Anuja Dampriya \nsenu gaming",inline=False)
-    myEmbed.add_field(name="Version code:",value="v1.0.0",inline=False)
+    myEmbed.add_field(name="University Of Moratuwa",value="Eshan \nUmal Manjitha \nGithmi Paranahewa \nVidith \nDilshan \nshehan \nPubudu \nhasathcharu \nHntr-Kllr \nThejan_B_W \nVidath Chamikara",inline=False)
+    myEmbed.add_field(name="Game Partners",value="Anuja Dampriya \nsenu gaming \nMikeHunt666 \n[SA81] ERA",inline=False)
+    myEmbed.add_field(name="Version code:",value="v2.0.0",inline=False)
     myEmbed.add_field(name="Date released",value="March 21,2019",inline=False)
-    myEmbed.set_image(url='https://cdn.discordapp.com/attachments/753250413058457712/823718466552332328/Screenshot_20210323-060920_1_1.png')
+    myEmbed.set_image(url='https://cdn.discordapp.com/attachments/673595467581358085/838903903868813362/giphy.gif')
     myEmbed.set_footer(text="Thank you.Have a nice day")
     myEmbed.set_author(name="Rise Of Kingdom Srilankan Official Discord Server")
 
@@ -26,6 +26,9 @@ async def info(context):
 
 @client.event
 async def on_ready():
+    activity = discord.Game(name="Rise Of Kingdom Srilanka", type=3)
+    await client.change_presence(status=discord.Status.idle, activity=activity)
+    print("Bot is ready!")
     #do stuff.....
     general_chanel = client.get_channel(753250363016216627)
     await general_chanel.send('HELLO WORLD')
@@ -35,25 +38,9 @@ async def on_disconnect():
 
     general_chanel = client.get_channel(753250363016216627)
     await general_chanel.send('good bye kattiya')
+   
 
-@client.event
-async def on_message(message):
 
-    if message.content == 'what is this':
-        general_chanel = client.get_channel(753250363016216627)
-
-        myEmbed = discord.Embed(title="Administrator",description="Nimash Eshan \nJohnny JTH",color=0x00ff00)
-        myEmbed.add_field(name="Partner",value="Githmi Paranahewa",inline=False)
-        myEmbed.add_field(name="Co leaders",value="Waruna Usgoda \nVasula \nnA PEACE \nRukshan \nmisha",inline=False)
-        myEmbed.add_field(name="Vip Members",value="Trey(ナ尺ツ) \nUmal Manjitha(bota malli) \nSisitha Liyanage(Fraddy Gaming \nBunny)",inline=False)
-        myEmbed.add_field(name="Guest",value="Anuja Dampriya \nsenu gaming",inline=False)
-        myEmbed.add_field(name="Version code:",value="v1.0.0",inline=False)
-        myEmbed.add_field(name="Date released",value="March 21,2019",inline=False)
-        myEmbed.set_footer(text="Thank you.Have a nice day")
-        myEmbed.set_author(name="Rise Of Kingdom Srilankan Official Discord Server")
-
-        await general_chanel.send(embed=myEmbed)
-    await client.process_commands(message)
 
 #run the client on the server
 client.run('ODIyOTY2OTcwMDY3ODQ1MTIw.YFZ9tw.v1JL2A9FanhTBNaskDFntmE06BU')
